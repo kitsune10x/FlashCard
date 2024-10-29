59f1fa94620f91b77f053323f58c9dc65e3063f9
kitsune10x <rapaz1203@gmail.com>
2024-10-29 11:11:31 -0300

    Add files via upload

---
+++ FlashCards-main/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação que é desejada com bastante frequência no mercado de trabalho'
+)
+
+criaCartao(
+    'Xadrez',
+    'Qual é o melhor jogador de xadrez da atualidade?',
+    'Magnus Carlsen'
+)
+
+criaCartao(
+    'Quimica',
+    'O que é cadeia carbônica?',
+    'Cadeia carbônica é uma sequência de átomos de carbonos ligados. "Cadeia" refere-se a estrutura que interliga os carbonos.'
+)
+
+criaCartao(
+    'Lingua Francesa',
+    'Como se diz olá em Francês?',
+    'Pode ser falado como bonjour'
+)
+
+criaCartao(
+    'Xadrez',
+    'Quem era o "Mago de Riga", o gênio dos sacríficios?',
+    'Mikhail Tal'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor país do mundo?',
+    'Vaticano, com uma área de 0,44 km² e mil habitantes'
+)
